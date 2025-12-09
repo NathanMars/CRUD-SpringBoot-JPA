c4fd3af684fdbfcc614894a156b65ccd604d74d5
NathanMars <nathanmarques.silva@gmail.com>
2025-12-09 10:10:09 -0300

    Feat: Adição de usuário e funcionário para testespor migration
    Adicionando o segundo arquivo de Migration com um usuário para login inicial e funcionário de exemplo

---
+++ backend/src/main/resources/db/migration/V2__seed_data.sql
@@ -0,0 +1,4 @@
+-- Password is 'admin' (BCrypt hash)
+INSERT INTO users (username, password) VALUES ('admin', '$2a$10$8.UnVuG9HHgffUDAlk8qfOpNaNSxFEAd4adeQd7IaqfRerF7joY.e');
+
+INSERT INTO employees (name, admission_date, salary, status) VALUES ('João da Silva', '2024-06-20', 3520.00, 'A');
