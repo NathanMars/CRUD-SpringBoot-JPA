cbb18d97ca504639794120057b26f20687f2a6ba
NathanMars <nathanmarques.silva@gmail.com>
2025-12-09 10:03:27 -0300

    Feat: Criando primeira Migration

---
+++ backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    admission_date DATE NOT NULL,
+    salary NUMERIC(19, 2) NOT NULL,
+    status CHAR(1) NOT NULL -- "A" (Ativo), "I" (Inativo), ultilizando CHAR(1) ao invés de BOOL pensando num futuro hipotético onde mais opções de estado são adicionadas, mas sem precisar usar ENUM do postgres, mantendo a simplicidade do projeto
+);
