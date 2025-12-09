b85805132da99d00e2ec88c91aa748eca589f6a6
NathanMars <nathanmarques.silva@gmail.com>
2025-12-09 11:07:37 -0300

    Fix: Correção de tipagem
    O uso de Long levava o Java a acreditar que a tabela continha um campo BigInt, mas o campo é na verdade Serial, causando erros. Alterei a tipagem para Integer para resolver o problema.

--- backend/src/main/java/sisaudcom/api/repository/UserRepository.java
+++ backend/src/main/java/sisaudcom/api/repository/UserRepository.java
@@ -7,6 +7,6 @@ import sisaudcom.api.entity.User;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
